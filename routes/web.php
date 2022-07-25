8467e4d7038de00d76724af313052ec74e01055e
Oskar <prukioksi@gmail.com>
2022-07-25 09:30:50 +0200

    Added user_id to items

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
+
 Route::get('/{any}', function () {
     return view('welcome');
-})->where("any",".*");
+})->where("any",".*")->middleware("auth:sanctum");
